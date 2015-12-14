380189b949365b0636347d4d04ac04b56df68b86
martincostello <martin@martincostello.com>
2015-12-14 20:15:35 +0000

    Solution for puzzle 6
    Find the difference between the sum of the squares of the first one
    hundred natural numbers and the square of the sum.

---
+++ src/ProjectEuler.Tests/Puzzles/Puzzle006Tests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle006"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle006Tests
+    {
+        [Theory]
+        [InlineData("10", 2640)]
+        [InlineData("100", 25164150)]
+        public static void Puzzle006_Returns_Correct_Solution(string value, int expected)
+        {
+            // Arrange
+            string[] args = new[] { value };
+            var target = new Puzzle006();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(0, actual);
+            Assert.Equal(expected, target.Answer);
+        }
+
+        [Fact]
+        public static void Puzzle006_Returns_Minus_One_If_Maximum_Value_Is_Invalid()
+        {
+            // Arrange
+            string[] args = new[] { "a" };
+            var target = new Puzzle006();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(-1, actual);
+        }
+
+        [Fact]
+        public static void Puzzle006_Returns_Minus_One_If_Maximum_Value_Is_Too_Small()
+        {
+            // Arrange
+            string[] args = new[] { "0" };
+            var target = new Puzzle006();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(-1, actual);
+        }
+    }
+}
---
+++ src/ProjectEuler/Puzzles/Puzzle006.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=6</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle006 : Puzzle
+    {
+        public override string Question => "What is the difference between the sum of the squares of the specified number of natural numbers and the square of their sum?";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 1;
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int numbers;
+
+            if (!int.TryParse(args[0], NumberStyles.Integer & ~NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out numbers) ||
+                numbers < 1)
+            {
+                Console.Error.WriteLine("The specified number is invalid.");
+                return -1;
+            }
+
+            var range = Enumerable.Range(1, numbers);
+
+            double sumOfSquares = range
+                .Select((p) => Math.Pow(p, 2))
+                .Sum();
+
+            double squareOfSum = Math.Pow(range.Sum(), 2);
+
+            Answer = (int)(squareOfSum - sumOfSquares);
+
+            return 0;
+        }
+    }
+}
