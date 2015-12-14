94404b5ba5e6994c10134de6f00ca7332317bd51
martincostello <martin@martincostello.com>
2015-12-14 17:44:30 +0000

    Solve problem 5
    What is the smallest positive number that is evenly divisible by all of
    the numbers from 1 to 20?

---
+++ src/ProjectEuler.Tests/Puzzles/Puzzle005Tests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle005"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle005Tests
+    {
+        [Theory]
+        [InlineData("10", 2520)]
+        [InlineData("20", 232792560)]
+        public static void Puzzle005_Returns_Correct_Solution(string max, int expected)
+        {
+            // Arrange
+            string[] args = new[] { max };
+            var target = new Puzzle005();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(0, actual);
+            Assert.Equal(expected, target.Answer);
+        }
+
+        [Fact]
+        public static void Puzzle005_Returns_Minus_One_If_Maximum_Value_Is_Invalid()
+        {
+            // Arrange
+            string[] args = new[] { "a" };
+            var target = new Puzzle005();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(-1, actual);
+        }
+
+        [Fact]
+        public static void Puzzle005_Returns_Minus_One_If_Maximum_Value_Is_Too_Small()
+        {
+            // Arrange
+            string[] args = new[] { "1" };
+            var target = new Puzzle005();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(-1, actual);
+        }
+    }
+}
---
+++ src/ProjectEuler/Puzzles/Puzzle005.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=5</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle005 : Puzzle
+    {
+        public override string Question => "What is the smallest positive number that is evenly divisible by all of the numbers from 1 to the specified value?";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 1;
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int max;
+
+            if (!int.TryParse(args[0], NumberStyles.Integer & ~NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out max) ||
+                max < 2)
+            {
+                Console.Error.WriteLine("The specified maximum number is invalid.");
+                return -1;
+            }
+
+            var divisors = Enumerable.Range(1, max).ToList();
+
+            for (int i = 1; i < int.MaxValue; i++)
+            {
+                if (divisors.All((p) => i % p == 0))
+                {
+                    Answer = i;
+                    break;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
