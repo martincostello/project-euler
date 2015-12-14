7f216158380366ac407a92e5d4aa0adfc99adad7
martincostello <martin@martincostello.com>
2015-12-14 17:15:46 +0000

    Add solution to problem 4
    Find the largest palindrome made from the product of two 3-digit
    numbers.

---
+++ src/ProjectEuler.Tests/Puzzles/Puzzle004Tests.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle004"/> class.
+    /// </summary>
+    public static class Puzzle004Tests
+    {
+        [Theory]
+        [InlineData("2", 9009)]
+        [InlineData("3", 906609)]
+        public static void Puzzle004_Returns_Correct_Solution(string digits, int expected)
+        {
+            // Arrange
+            string[] args = new[] { digits };
+
+            Puzzle004 target = new Puzzle004();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(0, actual);
+            Assert.Equal(expected, target.Answer);
+        }
+
+        [Fact]
+        public static void Puzzle004_Returns_Minus_One_If_Maximum_Value_Is_Invalid()
+        {
+            // Arrange
+            string[] args = new[] { "a" };
+
+            Puzzle004 target = new Puzzle004();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(-1, actual);
+        }
+
+        [Fact]
+        public static void Puzzle004_Returns_Minus_One_If_Maximum_Value_Is_Too_Small()
+        {
+            // Arrange
+            string[] args = new[] { "1" };
+
+            Puzzle004 target = new Puzzle004();
+
+            // Act
+            int actual = target.Solve(args);
+
+            // Assert
+            Assert.Equal(-1, actual);
+        }
+    }
+}
---
+++ src/ProjectEuler/Puzzles/Puzzle004.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=4</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle004 : Puzzle
+    {
+        public override string Question => "What is the largest palindrome made from the product of two numbers with the specified number of digits?";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 1;
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int digits;
+
+            if (!int.TryParse(args[0], NumberStyles.Integer & ~NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out digits) ||
+                digits < 2)
+            {
+                Console.Error.WriteLine("The specified number of digits is invalid.");
+                return -1;
+            }
+
+            var palindromes = new List<int>();
+
+            for (int i = (int)Math.Pow(10, digits - 1); i < Math.Pow(10, digits); i++)
+            {
+                for (int j = (int)Math.Pow(10, digits - 1); j < i + 1; j++)
+                {
+                    int product = i * j;
+
+                    char[] chars = product.ToString(CultureInfo.InvariantCulture).ToCharArray();
+
+                    bool isPalindromic = true;
+
+                    for (int k = 0; k < chars.Length / 2; k++)
+                    {
+                        if (chars[k] != chars[chars.Length - k - 1])
+                        {
+                            isPalindromic = false;
+                            break;
+                        }
+                    }
+
+                    if (isPalindromic)
+                    {
+                        palindromes.Add(product);
+                    }
+                }
+            }
+
+            Answer = palindromes.Max();
+
+            return 0;
+        }
+    }
+}
