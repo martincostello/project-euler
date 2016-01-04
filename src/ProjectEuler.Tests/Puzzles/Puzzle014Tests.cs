3114910b72b2e231863357653e6ef7aa3843fac4
martincostello <martin@martincostello.com>
2016-01-04 21:17:30 +0000

    Solve problem 14
    Which starting number, under one million, produces the longest chain?

---
+++ src/ProjectEuler.Tests/Puzzles/Puzzle014Tests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle014"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle014Tests
+    {
+        [Fact]
+        public static void Puzzle014_Returns_Correct_Solution()
+        {
+            // Arrange
+            object expected = 837799;
+
+            // Act and Assert
+            Puzzles.AssertSolution<Puzzle014>(expected);
+        }
+
+        [Fact]
+        public static void Puzzle_014_GetCollatzSequence_Returns_Correct_Sequence()
+        {
+            // Arrange
+            long start = 13;
+            var expected = new long[] { 13, 40, 20, 10, 5, 16, 8, 4, 2, 1 };
+
+            // Act
+            var actual = Puzzle014.GetCollatzSequence(start);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/ProjectEuler/Puzzles/Puzzle014.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=14</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle014 : Puzzle
+    {
+        /// <inheritdoc />
+        public override string Question => "Which starting number, under one million, produces the longest chain?";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 0;
+
+        /// <summary>
+        /// Gets the Collatz sequence that starts at the specified value.
+        /// </summary>
+        /// <param name="start">The start value.</param>
+        /// <returns>
+        /// An <see cref="IEnumerable{T}"/> that returns the Collatz sequence starting at <paramref name="start"/>.
+        /// </returns>
+        internal static IEnumerable<long> GetCollatzSequence(long start)
+        {
+            long current = start;
+
+            while (current != 1)
+            {
+                yield return current;
+
+                if (current % 2 == 0)
+                {
+                    current /= 2;
+                }
+                else
+                {
+                    current = (3 * current) + 1;
+                }
+            }
+
+            yield return current;
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int longestChain = 0;
+            int longestChainSeed = 0;
+
+            for (int i = 1; i < 1000000; i++)
+            {
+                int chainLength = GetCollatzSequence(i).Count();
+
+                if (chainLength > longestChain)
+                {
+                    longestChain = chainLength;
+                    longestChainSeed = i;
+                }
+            }
+
+            Answer = longestChainSeed;
+
+            return 0;
+        }
+    }
+}
