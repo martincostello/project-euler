2b2d27b32af2e8a7db35d3b1b4636462eb1780cb
martincostello <martin@martincostello.com>
2016-01-11 22:28:05 +0000

    Solve problem 19
    How many Sundays fell on the first of the month during the twentieth
    century (1 Jan 1901 to 31 Dec 2000)?

---
+++ src/ProjectEuler.Tests/Puzzles/Puzzle019Tests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle019"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle019Tests
+    {
+        [Fact]
+        public static void Puzzle019_Returns_Correct_Solution()
+        {
+            // Arrange
+            object expected = 171;
+
+            // Act and Assert
+            Puzzles.AssertSolution<Puzzle019>(expected);
+        }
+    }
+}
---
+++ src/ProjectEuler/Puzzles/Puzzle019.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=19</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle019 : Puzzle
+    {
+        /// <inheritdoc />
+        public override string Question => "How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 0;
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            DateTime date = new DateTime(1901, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            DateTime max = date.AddYears(100);
+
+            int sundaysOnTheFirstOfTheMonth = 0;
+
+            while (date < max)
+            {
+                if (date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    sundaysOnTheFirstOfTheMonth++;
+                }
+
+                date = date.AddMonths(1);
+            }
+
+            Answer = sundaysOnTheFirstOfTheMonth;
+
+            return 0;
+        }
+    }
+}
