c0c8e44e43b38ced6a0b068b46f66a458671d6b6
martincostello <martin@martincostello.com>
2016-01-11 23:30:30 +0000

    Solve puzzle 17
    If all the numbers from 1 to 1,000 inclusive were written out in words,
    how many letters would be used?

---
+++ src/ProjectEuler.Tests/Puzzles/Puzzle017Tests.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle017"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle017Tests
+    {
+        [Theory]
+        [InlineData(1, "one")]
+        [InlineData(7, "seven")]
+        [InlineData(22, "twenty-two")]
+        [InlineData(76, "seventy-six")]
+        [InlineData(101, "one hundred and one")]
+        [InlineData(110, "one hundred and ten")]
+        [InlineData(115, "one hundred and fifteen")]
+        [InlineData(150, "one hundred and fifty")]
+        [InlineData(342, "three hundred and forty-two")]
+        [InlineData(392, "three hundred and ninety-two")]
+        [InlineData(600, "six hundred")]
+        [InlineData(999, "nine hundred and ninety-nine")]
+        [InlineData(1000, "one thousand")]
+        [InlineData(4672, "four thousand six hundred and seventy-two")]
+        [InlineData(9999, "nine thousand nine hundred and ninety-nine")]
+        public static void Puzzle017_ToEnglish_Returns_Correct_Value(int value, string expected)
+        {
+            // Act
+            string actual = Puzzle017.ToEnglish(value);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("1", 3)]
+        [InlineData("5", 19)]
+        [InlineData("1000", 21124)]
+        public static void Puzzle017_Returns_Correct_Solution(string maximum, int expected)
+        {
+            // Arrange
+            string[] args = new[] { maximum };
+
+            // Act and Assert
+            Puzzles.AssertSolution<Puzzle017>(args, expected);
+        }
+
+        [Fact]
+        public static void Puzzle017_Returns_Minus_One_If_Maximum_Is_Invalid()
+        {
+            // Arrange
+            string[] args = new[] { "a" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle017>(args);
+        }
+
+        [Fact]
+        public static void Puzzle017_Returns_Minus_One_If_Maximum_Is_Too_Small()
+        {
+            // Arrange
+            string[] args = new[] { "0" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle017>(args);
+        }
+    }
+}
---
+++ src/ProjectEuler/Puzzles/Puzzle017.cs
@@ -0,0 +1,178 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Text;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=17</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle017 : Puzzle
+    {
+        /// <summary>
+        /// A dictionary containing the English words for numbers keyed by their value. This field is read-only.
+        /// </summary>
+        private static readonly Dictionary<int, string> NumberWords = new Dictionary<int, string>()
+        {
+            { 1, "one" },
+            { 2, "two" },
+            { 3, "three" },
+            { 4, "four" },
+            { 5, "five" },
+            { 6, "six" },
+            { 7, "seven" },
+            { 8, "eight" },
+            { 9, "nine" },
+            { 10, "ten" },
+            { 11, "eleven" },
+            { 12, "twelve" },
+            { 13, "thirteen" },
+            { 14, "fourteen" },
+            { 15, "fifteen" },
+            { 16, "sixteen" },
+            { 17, "seventeen" },
+            { 18, "eighteen" },
+            { 19, "nineteen" },
+            { 20, "twenty" },
+            { 30, "thirty" },
+            { 40, "forty" },
+            { 50, "fifty" },
+            { 60, "sixty" },
+            { 70, "seventy" },
+            { 80, "eighty" },
+            { 90, "ninety" },
+        };
+
+        /// <inheritdoc />
+        public override string Question => "If all the numbers from 1 to the specified value inclusive were written out in words, how many letters would be used?";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 1;
+
+        /// <summary>
+        /// Returns the British English representation of the specified number.
+        /// </summary>
+        /// <param name="value">The number to return the British English representation of.</param>
+        /// <returns>
+        /// he British English representation of <paramref name="value"/>.
+        /// </returns>
+        internal static string ToEnglish(int value)
+        {
+            string result;
+
+            if (NumberWords.TryGetValue(value, out result))
+            {
+                return result;
+            }
+
+            // 2345 -> 2 and 2000
+            int thousands = value / 1000;
+            int justTheThousands = thousands * 1000;
+
+            // 2345 -> 3 and 300
+            int hundreds = (value - justTheThousands) / 100;
+            int justTheHundreds = hundreds * 100;
+
+            // 2345 -> 4, 40 and 5
+            int justTheTens = value - justTheThousands - justTheHundreds;
+            int tens = justTheTens / 10;
+            int ones = justTheTens % 10;
+
+            StringBuilder builder = new StringBuilder();
+
+            if (thousands > 0)
+            {
+                builder
+                    .Append(NumberWords[thousands])
+                    .Append(" thousand");
+            }
+
+            if (hundreds > 0)
+            {
+                if (builder.Length > 0)
+                {
+                    builder.Append(' ');
+                }
+
+                builder
+                    .Append(NumberWords[hundreds])
+                    .Append(" hundred");
+            }
+
+            if (tens > 0)
+            {
+                if (builder.Length > 0)
+                {
+                    builder.Append(" and ");
+                }
+
+                int tenValue = tens * 10;
+
+                // Is this a special "ten" like "fifteen"?
+                if (NumberWords.TryGetValue(tenValue + ones, out result))
+                {
+                    builder.Append(result);
+
+                    // Clear the ones as we do not need to print them
+                    ones = 0;
+                }
+                else
+                {
+                    builder.Append(NumberWords[tenValue]);
+                }
+            }
+
+            if (ones > 0)
+            {
+                if (builder.Length > 0)
+                {
+                    if (tens < 1)
+                    {
+                        // Values like "three hundred and one"
+                        builder.Append(" and ");
+                    }
+                    else
+                    {
+                        // Values like "eighty-six"
+                        builder.Append('-');
+                    }
+                }
+
+                builder.Append(NumberWords[ones]);
+            }
+
+            return builder.ToString();
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int maximum;
+
+            if (!int.TryParse(args[0], NumberStyles.Integer & ~NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out maximum) ||
+                maximum < 1)
+            {
+                Console.Error.WriteLine("The specified number is invalid.");
+                return -1;
+            }
+
+            int sum = 0;
+
+            for (int i = 1; i <= maximum; i++)
+            {
+                sum += ToEnglish(i)
+                    .Replace(" ", string.Empty)
+                    .Replace("-", string.Empty)
+                    .Length;
+            }
+
+            Answer = sum;
+
+            return 0;
+        }
+    }
+}
