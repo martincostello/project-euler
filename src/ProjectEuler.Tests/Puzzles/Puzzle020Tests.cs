c8257a9fd7b626e862ccbd2804cc00e8d98b3f91
martincostello <martin@martincostello.com>
2016-01-12 20:59:01 +0000

    Solve puzzle 20
    Find the sum of the digits of 100 factorial (100!).

---
+++ src/ProjectEuler.Tests/Puzzles/Puzzle020Tests.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle020"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle020Tests
+    {
+        [Theory]
+        [InlineData("0", 1)]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        [InlineData("3", 6)]
+        [InlineData("4", 6)]
+        [InlineData("10", 27)]
+        [InlineData("100", 648)]
+        public static void Puzzle020_Returns_Correct_Solution(string number, int expected)
+        {
+            // Arrange
+            string[] args = new[] { number };
+
+            // Act and Assert
+            Puzzles.AssertSolution<Puzzle020>(args, expected);
+        }
+
+        [Fact]
+        public static void Puzzle020_Returns_Minus_One_If_Number_Is_Invalid()
+        {
+            // Arrange
+            string[] args = new[] { "a" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle020>(args);
+        }
+
+        [Fact]
+        public static void Puzzle020_Returns_Minus_One_If_Number_Is_Too_Small()
+        {
+            // Arrange
+            string[] args = new[] { "-1" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle020>(args);
+        }
+    }
+}
---
+++ src/ProjectEuler/Puzzles/Puzzle020.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=16</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle020 : Puzzle
+    {
+        /// <inheritdoc />
+        public override string Question => "Find the sum of the digits in the factorial for the specified number (i.e. N!).";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 1;
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int number;
+
+            if (!int.TryParse(args[0], NumberStyles.Integer & ~NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture, out number) ||
+                number < 0)
+            {
+                Console.Error.WriteLine("The specified number is invalid.");
+                return -1;
+            }
+
+            string factorial = string.Empty;
+
+            if (number > 0)
+            {
+                factorial = number.ToString(CultureInfo.InvariantCulture);
+
+                // For n -> 0
+                for (int n = number; n > 0; n--)
+                {
+                    string current = factorial;
+
+                    // Add n to the result n-1 times
+                    for (int j = 1; j < n - 1; j++)
+                    {
+                        factorial = Maths.Add(factorial, current);
+                    }
+                }
+            }
+
+            Answer = factorial
+                .Select((p) => p - '0')
+                .DefaultIfEmpty(1)
+                .Sum();
+
+            return 0;
+        }
+    }
+}
