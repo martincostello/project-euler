734e08b4896c13b8ec387904d81af32c59b91595
martincostello <martin@martincostello.com>
2016-01-13 13:33:29 +0000

    Solve problem 25
    What is the index of the first term in the Fibonacci sequence to contain
    1,000 digits?

---
+++ src/ProjectEuler.Tests/Puzzles/Puzzle025Tests.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System.Linq;
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle025"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle025Tests
+    {
+        [Fact]
+        public static void Puzzle025_Returns_Correct_Solution()
+        {
+            // Arrange
+            int expected = 4782;
+
+            // Act and Assert
+            Puzzles.AssertSolution<Puzzle025>(expected);
+        }
+
+        [Fact]
+        public static void Puzzle025_Fibonacci_Returns_Correct_Sequence()
+        {
+            // Arrange
+            var expected = new[] { "1", "1", "2", "3", "5", "8", "13", "21", "34", "55", "89", "144" };
+
+            // Act
+            var actual = Puzzle025.Fibonacci()
+                .Take(expected.Length)
+                .ToArray();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/ProjectEuler/Puzzles/Puzzle025.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=25</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle025 : Puzzle
+    {
+        /// <inheritdoc />
+        public override string Question => "What is the number of the first term in the Fibonacci sequence to contain 1,000 digits?";
+
+        /// <summary>
+        /// Returns an <see cref="IEnumerable{T}"/> of <see cref="string"/> that enumerates the Fibonacci sequence.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="IEnumerable{T}"/> of <see cref="string"/> that enumerates the Fibonacci sequence.
+        /// </returns>
+        internal static IEnumerable<string> Fibonacci()
+        {
+            List<string> sequence = new List<string>() { "1", "1" };
+
+            foreach (string value in sequence)
+            {
+                yield return value;
+            }
+
+            for (int i = 2; ; i++)
+            {
+                string value = Maths.Add(sequence[i - 1], sequence[i - 2]);
+
+                yield return value;
+
+                sequence.Add(value);
+            }
+        }
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int index = 0;
+
+            foreach (string value in Fibonacci())
+            {
+                index++;
+
+                if (value.Length == 1000)
+                {
+                    Answer = index;
+                    break;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
