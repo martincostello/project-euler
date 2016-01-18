ebd1f6c23886fecc4812f6357ce21b7a0b3653a2
martincostello <martin@martincostello.com>
2016-01-18 18:35:22 +0000

    Cache the results of D()
    Cache the results of the Puzzle021.D() method.

--- src/ProjectEuler/Puzzles/Puzzle021.cs
+++ src/ProjectEuler/Puzzles/Puzzle021.cs
@@ -11,6 +11,11 @@ namespace MartinCostello.ProjectEuler.Puzzles
     /// </summary>
     internal sealed class Puzzle021 : Puzzle
     {
+        /// <summary>
+        /// A cache of the result of invocations of <see cref="D(long)"/>. This field is read-only.
+        /// </summary>
+        private static readonly IDictionary<long, long> _cache = new Dictionary<long, long>();
+
         /// <inheritdoc />
         public override string Question => "Evaluate the sum of all the amicable numbers under 10,000.";
 
@@ -52,6 +57,16 @@ namespace MartinCostello.ProjectEuler.Puzzles
         /// <returns>
         /// The sum of the proper divisors of <paramref name="value"/>.
         /// </returns>
-        private static long D(long value) => Maths.GetProperDivisors(value).Sum();
+        private static long D(long value)
+        {
+            long result;
+
+            if (_cache.TryGetValue(value, out result))
+            {
+                return result;
+            }
+
+            return _cache[value] = Maths.GetProperDivisors(value).Sum();
+        }
     }
 }
