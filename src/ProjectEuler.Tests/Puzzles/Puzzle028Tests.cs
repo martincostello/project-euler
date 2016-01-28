89b2f7de15a416bfd1f07f686e538a2a423639ae
martincostello <martin@martincostello.com>
2016-01-28 23:38:39 +0000

    Solve puzzle 28
    What is the sum of the numbers on the diagonals in a 1001 by 1001
    spiral?

---
+++ src/ProjectEuler.Tests/Puzzles/Puzzle028Tests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle028"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle028Tests
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("3", 25)]
+        [InlineData("5", 101)]
+        [InlineData("7", 261)]
+        [InlineData("1001", 669171001)]
+        public static void Puzzle028_Returns_Correct_Solution(string sides, int expected)
+        {
+            // Arrange
+            string[] args = new[] { sides };
+
+            // Act and Assert
+            Puzzles.AssertSolution<Puzzle028>(args, expected);
+        }
+
+        [Fact]
+        public static void Puzzle028_Returns_Minus_One_If_Sides_Value_Is_Invalid()
+        {
+            // Arrange
+            string[] args = new[] { "a" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle028>(args);
+        }
+
+        [Fact]
+        public static void Puzzle028_Returns_Minus_One_If_Sides_Value_Is_Too_Small()
+        {
+            // Arrange
+            string[] args = new[] { "0" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle028>(args);
+        }
+
+        [Fact]
+        public static void Puzzle028_Returns_Minus_One_If_Sides_Value_Is_Even()
+        {
+            // Arrange
+            string[] args = new[] { "2" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle028>(args);
+        }
+    }
+}
---
+++ src/ProjectEuler/Puzzles/Puzzle028.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=28</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle028 : Puzzle
+    {
+        /// <inheritdoc />
+        public override string Question => "What is the sum of the numbers on the diagonals in a square spiral with the specified number of sides?";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 1;
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int sides;
+
+            if (!TryParseInt32(args[0], out sides) || sides < 1 || sides % 2 == 0)
+            {
+                Console.Error.WriteLine("The specified number of sides is invalid.");
+                return -1;
+            }
+
+            int sum = 1;
+            int ringStart = 2;
+
+            for (int sideLength = 3; ringStart < sides * sides; sideLength += 2)
+            {
+                int lastCorner = sideLength * sideLength;
+                int thirdCorner = lastCorner - sideLength + 1;
+                int secondCorner = thirdCorner - sideLength + 1;
+                int firstCorner = secondCorner - sideLength + 1;
+
+                sum += firstCorner;
+                sum += secondCorner;
+                sum += thirdCorner;
+                sum += lastCorner;
+
+                ringStart = lastCorner + 1;
+            }
+
+            Answer = sum;
+
+            return 0;
+        }
+    }
+}
