8d11d3a31af5ff83e13f13b3187dd4a120cdf5e9
martincostello <martin@martincostello.com>
2016-01-30 15:53:31 +0000

    Partial solution to problem 48
    Partial solution to problem 48 which solves the problem for small
    numbers (i.e. 10), but not for the target number of 1000^1000, for which
    it is too slow.

---
+++ src/ProjectEuler/Puzzles/Puzzle048.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=48</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle048 : Puzzle
+    {
+        /// <inheritdoc />
+        public override string Question => "Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + N^N.";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 1;
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int n;
+
+            if (!TryParseInt32(args[0], out n) || n < 2)
+            {
+                Console.Error.WriteLine("The specified number is invalid.");
+                return -1;
+            }
+
+            Answer = Enumerable.Range(1, n)
+                .Select((p) => Maths.Pow(p.ToString(CultureInfo.InvariantCulture), p))
+                .Aggregate((x, y) => Maths.Sum(x, y));
+
+            return 0;
+        }
+    }
+}
