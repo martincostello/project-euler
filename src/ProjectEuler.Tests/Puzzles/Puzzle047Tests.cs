8991fbe133fbdf6a1344047d9a7a21ea3e7049eb
martincostello <martin@martincostello.com>
2016-01-30 15:39:24 +0000

    Solve puzzle 47
    Find the first four consecutive integers to have four distinct prime
    factors. What is the first of these numbers?

---
+++ src/ProjectEuler.Tests/Puzzles/Puzzle047Tests.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle047"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle047Tests
+    {
+        [Theory]
+        [InlineData("2", 14)]
+        [InlineData("3", 644)]
+        [InlineData("4", 134043)]
+        public static void Puzzle047_Returns_Correct_Solution(string factors, int expected)
+        {
+            // Arrange
+            string[] args = new[] { factors };
+
+            // Act and Assert
+            Puzzles.AssertSolution<Puzzle047>(args, expected);
+        }
+
+        [Fact]
+        public static void Puzzle047_Returns_Minus_One_If_Factors_Value_Is_Invalid()
+        {
+            // Arrange
+            string[] args = new[] { "a" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle047>(args);
+        }
+
+        [Fact]
+        public static void Puzzle047_Returns_Minus_One_If_Factors_Value_Is_Too_Small()
+        {
+            // Arrange
+            string[] args = new[] { "1" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle047>(args);
+        }
+    }
+}
---
+++ src/ProjectEuler/Puzzles/Puzzle047.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=47</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle047 : Puzzle
+    {
+        /// <inheritdoc />
+        public override string Question => "Find the first N consecutive integers to have N distinct prime factors. What is the first of these numbers?";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 1;
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int factors;
+
+            if (!TryParseInt32(args[0], out factors) || factors < 2)
+            {
+                Console.Error.WriteLine("The specified number of factors is invalid.");
+                return -1;
+            }
+
+            for (int i = 1; ; i++)
+            {
+                if (Enumerable.Range(i, factors).All((p) => HasPrimeFactors(p, factors)))
+                {
+                    Answer = i;
+                    break;
+                }
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns whether the specified value has the specified
+        /// number of factors that are prime numbers.
+        /// </summary>
+        /// <param name="value">The value to test.</param>
+        /// <param name="count">The number of prime factors to test for.</param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="value"/> has exactly
+        /// <paramref name="count"/> prime factors; otherwise <see langword="false"/>.
+        /// </returns>
+        private static bool HasPrimeFactors(int value, int count)
+        {
+            return Maths.GetFactorsUnordered(value)
+                .Where(Maths.IsPrime)
+                .Count() == count;
+        }
+    }
+}
