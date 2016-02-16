b2409d537fd12fdea1043f1bac92c145cc9a29e8
martincostello <martin@martincostello.com>
2016-02-16 18:26:24 +0000

    Solve problem 31
    How many different ways can £2 be made using any number of coins?

---
+++ src/ProjectEuler.Tests/Puzzles/Puzzle031Tests.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle031"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle031Tests
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        [InlineData("3", 2)]
+        [InlineData("4", 3)]
+        [InlineData("200", 73682)]
+        public static void Puzzle031_Returns_Correct_Solution(string total, int expected)
+        {
+            // Arrange
+            string[] args = new[] { total };
+
+            // Act and Assert
+            Puzzles.AssertSolution<Puzzle031>(args, expected);
+        }
+
+        [Fact]
+        public static void Puzzle031_Returns_Minus_One_If_Total_Is_Invalid()
+        {
+            // Arrange
+            string[] args = new[] { "a" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle031>(args);
+        }
+
+        [Fact]
+        public static void Puzzle031_Returns_Minus_One_If_Total_Is_Too_Small()
+        {
+            // Arrange
+            string[] args = new[] { "-1" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle031>(args);
+        }
+    }
+}
---
+++ src/ProjectEuler/Puzzles/Puzzle031.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=31</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle031 : Puzzle
+    {
+        /// <summary>
+        /// The coins, in pence, available in GBP. This field is read-only.
+        /// </summary>
+        private static readonly int[] Coins = new[] { 1, 2, 5, 10, 20, 50, 100, 200 };
+
+        /// <inheritdoc />
+        public override string Question => "How many different ways can the specified amount of money, in pence, be made using any number of coins?";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 1;
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int target;
+
+            if (!TryParseInt32(args[0], out target) || target < 1)
+            {
+                Console.Error.WriteLine("The specified total is invalid.");
+                return -1;
+            }
+
+            // Filter out the coins that are bigger than the target
+            int[] usefulCoins = Coins
+                .Where((p) => p <= target)
+                .ToArray();
+
+            // Array containing the number of ways there are to
+            // make 1p up to the target using the avaiable coins.
+            int[] ways = new int[target + 1];
+
+            // There is one way to make 1p
+            ways[0] = 1;
+
+            // Loop through each value of coin
+            for (int coin = 0; coin < usefulCoins.Length; coin++)
+            {
+                int coinValue = usefulCoins[coin];
+
+                // For each value from the coin's value to the target
+                // find how many ways there are to create that value.
+                for (int j = coinValue; j <= target; j++)
+                {
+                    int valueRemaining = j - coinValue;
+                    ways[j] += ways[valueRemaining];
+                }
+            }
+
+            Answer = ways[target];
+
+            return 0;
+        }
+    }
+}
