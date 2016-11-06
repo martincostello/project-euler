6f86ef6ce7c324074634e3709ae78e520713b53d
martincostello <martin@martincostello.com>
2016-11-06 13:37:17 +0000

    Initial attempt to solve puzzle 50
    An initial attempt at solving puzzle 50.
    It works for the stated example, but does not scale to the value
    required to solve for 1,000,000.

---
+++ src/ProjectEuler/Puzzles/Puzzle050.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// A class representing the solution to <c>https://projecteuler.net/problem=50</c>. This class cannot be inherited.
+    /// </summary>
+    internal sealed class Puzzle050 : Puzzle
+    {
+        /// <inheritdoc />
+        public override string Question => "Which prime, below the specified value, can be written as the sum of the most consecutive primes?";
+
+        /// <inheritdoc />
+        protected override int MinimumArguments => 1;
+
+        /// <inheritdoc />
+        protected override int SolveCore(string[] args)
+        {
+            int maximum;
+
+            if (!TryParseInt32(args[0], out maximum) || maximum < 6)
+            {
+                Console.WriteLine("The specified number is invalid.");
+                return -1;
+            }
+
+            IList<int> primes = Maths.Primes(maximum)
+                .Reverse()
+                .ToList();
+
+            int answer = 0;
+            int longestSum = 0;
+
+            foreach (int prime in primes)
+            {
+                IList<int> lesserPrimes = primes
+                    .Where((p) => p < prime)
+                    .ToList();
+
+                if (lesserPrimes.Count < 2 ||
+                    (longestSum != 0 && lesserPrimes.Count < longestSum))
+                {
+                    break;
+                }
+
+                if (lesserPrimes.Count > longestSum)
+                {
+                    for (int i = 0; i < lesserPrimes.Count; i++)
+                    {
+                        IList<int> slice = lesserPrimes
+                            .Skip(i)
+                            .Reverse()
+                            .ToList();
+
+                        int sum = prime;
+                        int used = 0;
+
+                        for (int j = 0; j < slice.Count && sum > 0; j++)
+                        {
+                            sum -= slice[j];
+                            used++;
+                        }
+
+                        if (sum == 0 && used > longestSum)
+                        {
+                            answer = prime;
+                            longestSum = used;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Answer = answer;
+
+            return 0;
+        }
+    }
+}
---
+++ tests/ProjectEuler.Tests/Puzzles/Puzzle050Tests.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle050"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle050Tests
+    {
+        [Theory]
+        [InlineData("6", 5)]
+        [InlineData("100", 41)]
+        /*[InlineData("1000000", 0)]*/
+        public static void Puzzle050_Returns_Correct_Solution(string maximum, int expected)
+        {
+            // Arrange
+            string[] args = new[] { maximum };
+
+            // Act and Assert
+            Puzzles.AssertSolution<Puzzle050>(args, expected);
+        }
+
+        [Fact]
+        public static void Puzzle050_Returns_Minus_One_If_Maximum_Is_Invalid()
+        {
+            // Arrange
+            string[] args = new[] { "a" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle050>(args);
+        }
+
+        [Fact]
+        public static void Puzzle050_Returns_Minus_One_If_Maximum_Is_Too_Small()
+        {
+            // Arrange
+            string[] args = new[] { "2" };
+
+            // Act and Assert
+            Puzzles.AssertInvalid<Puzzle050>(args);
+        }
+    }
+}
