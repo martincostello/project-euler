b2cedbd15082177a84d7c22f21bd93caad2c823c
martincostello <martin@martincostello.com>
2017-01-30 21:06:53 +0000

    Fully qualify project paths

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ﻿#!/bin/sh
 dotnet restore --verbosity minimal || exit 1
-dotnet build src/ProjectEuler || exit 1
-dotnet test tests/ProjectEuler.Tests || exit 1
+dotnet build src/ProjectEuler/ProjectEuler.csproj || exit 1
+dotnet test tests/ProjectEuler.Tests/ProjectEuler.Tests.csproj || exit 1
