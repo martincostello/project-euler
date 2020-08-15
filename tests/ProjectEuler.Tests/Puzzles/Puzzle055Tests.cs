08f1fa5d2c950bf095eb53b1570183ab25db91eb
martincostello <martin@martincostello.com>
2020-08-15 20:04:05 +0100

    Solve problem 55
    How many Lychrel numbers are there below ten-thousand?

---
+++ tests/ProjectEuler.Tests/Puzzles/Puzzle055Tests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.ProjectEuler.Puzzles
+{
+    using Xunit;
+
+    /// <summary>
+    /// A class containing tests for the <see cref="Puzzle055"/> class. This class cannot be inherited.
+    /// </summary>
+    public static class Puzzle055Tests
+    {
+        [Fact]
+        public static void Puzzle055_Returns_Correct_Solution()
+        {
+            // Arrange
+            int expected = 249;
+
+            // Act and Assert
+            Puzzles.AssertSolution<Puzzle055>(expected);
+        }
+    }
+}
