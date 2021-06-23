4d266f9f9853b3bf9258b3a3bbaf0d05898f6a32
martincostello <martin@martincostello.com>
2021-06-23 11:06:01 +0100

    Add global usings
    Add global usings with C# 10.

--- tests/ProjectEuler.Tests/Puzzles/Puzzle029Tests.cs
+++ tests/ProjectEuler.Tests/Puzzles/Puzzle029Tests.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Collections.Generic;
-using System.Linq;
-using Xunit;
-
 namespace MartinCostello.ProjectEuler.Puzzles
 {
     /// <summary>
