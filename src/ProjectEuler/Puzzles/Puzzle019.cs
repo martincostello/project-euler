4ef44f58b8672efe74eac9505ee3e10773c838cc
martincostello <martin@martincostello.com>
2023-05-17 14:28:07 +0100

    Use DateOnly
    Use `DateOnly` instead of NodaTime.

--- src/ProjectEuler/Puzzles/Puzzle019.cs
+++ src/ProjectEuler/Puzzles/Puzzle019.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Martin Costello, 2015. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using NodaTime;
-
 namespace MartinCostello.ProjectEuler.Puzzles;
 
 /// <summary>
@@ -16,19 +14,19 @@ public sealed class Puzzle019 : Puzzle
     /// <inheritdoc />
     protected override int SolveCore(string[] args)
     {
-        var date = new LocalDate(1901, 1, 1);
-        var max = date.PlusYears(100);
+        var date = new DateOnly(1901, 1, 1);
+        var max = date.AddYears(100);
 
         int sundaysOnTheFirstOfTheMonth = 0;
 
         while (date < max)
         {
-            if (date.DayOfWeek == IsoDayOfWeek.Sunday)
+            if (date.DayOfWeek == DayOfWeek.Sunday)
             {
                 sundaysOnTheFirstOfTheMonth++;
             }
 
-            date = date.PlusMonths(1);
+            date = date.AddMonths(1);
         }
 
         Answer = sundaysOnTheFirstOfTheMonth;
