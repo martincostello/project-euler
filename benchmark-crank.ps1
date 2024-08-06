920d12eef67d3c6eb8ebe60d628413bcffbb0450
martincostello <martin@martincostello.com>
2024-08-06 17:10:00 +0100

    Add crank benchmarks
    Add assets to run crank benchmarks in GitHub Actions.

---
+++ benchmark-crank.ps1
@@ -0,0 +1,60 @@
+#! /usr/bin/env pwsh
+
+#Requires -PSEdition Core
+#Requires -Version 7
+
+param(
+    [Parameter(Mandatory = $true)][string] $PullRequestId,
+    [Parameter(Mandatory = $false)][string] $AccessToken,
+    [Parameter(Mandatory = $false)][string] $Benchmark = "microbenchmarks",
+    [Parameter(Mandatory = $false)][string] $CrankPath = "",
+    [Parameter(Mandatory = $false)][string] $Repository = "https://github.com/martincostello/project-euler",
+    [Parameter(Mandatory = $false)][switch] $PublishResults
+)
+
+$crankAgent = "crank-agent"
+$crankPR = "crank-pr"
+
+if (![string]::IsNullOrEmpty($CrankPath)) {
+    $crankAgent = Join-Path $CrankPath $crankAgent
+    $crankPR = Join-Path $CrankPath $crankPR
+}
+
+if ($IsWindows) {
+    $agent = Start-Process -FilePath $crankAgent -WindowStyle Hidden -PassThru
+} else {
+    $agent = Start-Process -FilePath $crankAgent -PassThru
+}
+
+Start-Sleep -Seconds 2
+
+$repoPath = $PSScriptRoot
+$components = "app"
+$config = Join-Path $repoPath "benchmark.yml"
+$profiles = "local"
+
+$additionalArgs = @()
+
+if (![string]::IsNullOrEmpty($AccessToken)) {
+    $additionalArgs += "--access-token"
+    $additionalArgs += $AccessToken
+
+    if ($PublishResults) {
+        $additionalArgs += "--publish-results"
+        $additionalArgs += "true"
+    }
+}
+
+try {
+    & $crankPR `
+        --benchmarks $Benchmark `
+        --components $components `
+        --config $config `
+        --profiles $profiles `
+        --pull-request $PullRequestId `
+        --repository $Repository `
+        $additionalArgs
+}
+finally {
+    Stop-Process -InputObject $agent -Force | Out-Null
+}
